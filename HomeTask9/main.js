127d0b28ea681404424502514fd760bdf8f4889b
Virilis <vbokachev@gmail.com>
2019-12-03 22:21:32 +0200

    change

--- HomeTask9/main.js
+++ HomeTask9/main.js
@@ -1,17 +1,16 @@
 //********************** Task 1 ********************** 
 
-/*$("a[href^='https']").attr("target", "_blank");*/
+$("a[href^='https']").attr("target", "_blank");
 
 //********************** Task 2 ********************** 
 
-/*let $el=$('h2.head');
-$el.css('background','green');
-$el.children(".inner").css('font-size','35px');*/
+$('h2.head').css('background','green');
+$('h2.head').children(".inner").css('font-size','35px');
 
 //********************** Task 2 ********************** 
-/*let $elDiv = $("h3").next('div')
+
+let $elDiv = $("h3").next('div')
 $('h3').before(function(i){return $elDiv[i]})
-*/
 
 //********************** Task 2 ********************** 
 
